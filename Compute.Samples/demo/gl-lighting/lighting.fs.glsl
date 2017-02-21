c89374bbbcbd0f1c203836801ee27d89790e0145
William Taylor <wi11berto@yahoo.co.uk>
2017-02-21 12:00:47 +0000

    adding more to the report + updating lighting shader

--- Compute.Samples/demo/gl-lighting/lighting.fs.glsl
+++ Compute.Samples/demo/gl-lighting/lighting.fs.glsl
@@ -1,6 +1,7 @@
 #version 330
 
-out vec4 color;
+out vec4 colour;
+
 in vec3 normal;
 
 struct DirectionalLight 
@@ -12,13 +13,11 @@ struct DirectionalLight
 
 void main()
 {
-  vec4 surfaceColour = vec4(1.0, 0.0, 0.0, 1.0);
-
   DirectionalLight sun; 
-  sun.colour = vec3(1.0, 0.0, 0.0);
   sun.direction = normalize(vec3(-5.0, -1.0, 0.0));
+  sun.colour = vec3(1.0, 0.0, 0.0);
   sun.ambient = 0.25;
 
   float diffuse = max(0.0, dot(normalize(normal), -sun.direction));
-  color = surfaceColour * vec4(sun.colour * (sun.ambient + diffuse), 1.0);
+  colour = vec4(1.0, 0.0, 0.0, 1.0) * vec4(sun.colour * (sun.ambient + diffuse), 1.0);
 }
