9722f1cb93a18299e0ace3a76ce42db4da1f00b0
william-taylor <wi11berto@yahoo.co.uk>
2017-03-11 17:51:02 +0000

    cleaning up lighting demo

--- Compute.Samples/demo/gl-lighting/app.js
+++ Compute.Samples/demo/gl-lighting/app.js
@@ -1,13 +1,12 @@
 
 const { openWindow } = require('display');
-const console = require('console');
 const maths = require('maths');
 const gl = require('gl');
 const fs = require('fs');
 
 const geometry = fs.readJson('./cube.json');
-const shaders = { 
-    vs: fs.read('lighting.vs.glsl'), 
+const shaders = {
+    vs: fs.read('lighting.vs.glsl'),
     fs: fs.read('lighting.fs.glsl')
 };
 
@@ -16,59 +15,58 @@ function createShader(shaderType, shaderSource) {
         const shader = glCreateShader(shaderType);
         glShaderSource(shader, 1, shaderSource, 0);
         glCompileShader(shader);
-
-        const compileStatus = [,], logLength = [,];
-        glGetShaderiv(shader, GL_COMPILE_STATUS, compileStatus);
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, logLength);
-
-        if (compileStatus[0] != GL_TRUE) {
-            const log = [,];
-            glGetShaderInfoLog(shader, logLength[0], logLength[0], log)
-            console.log('Compile error', log[0]);
-        }
-
         return shader;
     }
 }
 
-openWindow({ x: 450, y: 250, w: 800, h: 600 }, window => {
-    window.setTitle('Basic Lighting');
-    window.show();
-    window.enableOpenGL();
+function configureDisplay(callback) {
+    openWindow({ x: 450, y: 250, w: 800, h: 600 }, window => {
+        window.setTitle('Basic Lighting');
+        window.show();
+        window.enableOpenGL();
 
+        callback(window);
+    });
+}
+
+function createProgram() {
     with (gl) {
         const vs = createShader(GL_VERTEX_SHADER, shaders.vs.contents);
         const fs = createShader(GL_FRAGMENT_SHADER, shaders.fs.contents);
+
         const program = glCreateProgram();
-        
         glAttachShader(program, vs);
         glAttachShader(program, fs);
         glLinkProgram(program);
         glUseProgram(program);
-        glEnable(GL_DEPTH_TEST);
+        return program;
+    }
+}
 
-        const vertexArrayObject = new Uint32Array(1);
+function createBuffer(data, index, count) {
+    with (gl) {
         const vertexBuffer = new Uint32Array(1);
-        const normalBuffer = new Uint32Array(1);
-        const vertexData = Float32Array.from(geometry.cube);
-        const normalData = Float32Array.from(geometry.normals);
-
-        glGenVertexArray(1, vertexArrayObject);
-        glBindVertexArray(vertexArrayObject[0]);
-
         glGenBuffers(1, vertexBuffer);
         glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer[0]);
-        glBufferData(GL_ARRAY_BUFFER, vertexData.byteLength, vertexData, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glBufferData(GL_ARRAY_BUFFER, data.byteLength, data, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(index);
+        glVertexAttribPointer(index, count, GL_FLOAT, GL_FALSE, 0, 0);
+        return vertexBuffer;
+    }
+}
 
-        glGenBuffers(1, normalBuffer);
-        glBindBuffer(GL_ARRAY_BUFFER, normalBuffer[0]);
-        glBufferData(GL_ARRAY_BUFFER, normalData.byteLength, normalData, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+configureDisplay(window => {
+    let projection = maths.mat4.create(), view = maths.mat4.create();
+    let program = createProgram(), vao = new Uint32Array(1);
+    let rotation = 0;
 
-        const projection = maths.mat4.create(), view = maths.mat4.create();
+    with (gl) {
+        glGenVertexArray(1, vao);
+        glBindVertexArray(vao[0]);
+        glEnable(GL_DEPTH_TEST);
+
+        createBuffer(Float32Array.from(geometry.cube), 0, 3);
+        createBuffer(Float32Array.from(geometry.normals), 1, 3);
 
         maths.mat4.perspective(projection, maths.glMatrix.toRadian(45.0), 4.0 / 3.0, 0.1, 100.0);
         maths.mat4.lookAt(view,
@@ -77,8 +75,6 @@ openWindow({ x: 450, y: 250, w: 800, h: 600 }, window => {
             maths.vec3.fromValues(0, 1, 0)
         );
 
-        let rotation = 0;
-
         window.onFrame(() => {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             glClearColor(0.2, 0.2, 0.2, 0.2);
@@ -86,17 +82,17 @@ openWindow({ x: 450, y: 250, w: 800, h: 600 }, window => {
             const projectionLocation = glGetUniformLocation(program, "projection");
             const modelLocation = glGetUniformLocation(program, "model");
             const viewLocation = glGetUniformLocation(program, "view");
-
             const model = maths.mat4.create();
+
             maths.mat4.rotate(model, model, maths.glMatrix.toRadian(rotation), maths.vec3.fromValues(0.0, -1.0, 0.0));
-            rotation += 0.25;
 
             glUniformMatrix4(projectionLocation, 1, GL_FALSE, Float32Array.from(projection));
             glUniformMatrix4(modelLocation, 1, GL_FALSE, Float32Array.from(model));
             glUniformMatrix4(viewLocation, 1, GL_FALSE, Float32Array.from(view));
-            glDrawArrays(GL_TRIANGLES, 0, vertexData.length / 3);
-            
+            glDrawArrays(GL_TRIANGLES, 0, geometry.cube.length / 3);
+
             window.swapBuffers();
+            rotation += 0.25;
         });
     }
 });
